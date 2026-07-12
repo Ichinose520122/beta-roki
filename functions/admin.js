4c199433af2db6827c892bd11c60ec365d19ac76
Ichinose520122 <ichinose520122@gmail.com>
2026-07-12 17:32:53 +0800

    Update admin.js

--- functions/admin.js
+++ functions/admin.js
@@ -1,4 +1,9 @@
 export function onRequestGet(context) {
-  const target = new URL("/admin/", context.request.url);
-  return Response.redirect(target.toString(), 308);
+  const url = new URL(context.request.url);
+  if (url.pathname === "/admin") {
+    const target = new URL("/admin/", context.request.url);
+    return Response.redirect(target.toString(), 308);
+  }
+
+  return context.env.ASSETS.fetch(context.request);
 }
